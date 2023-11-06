0819baa9418fcfcd6ee64ca271929ab44d1b8ae1
Sam Campbell <samcam@umich.edu>
2023-11-06 14:17:52 -0500

    Final Commit

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -1,10 +1,13 @@
+const myArr = [];
+
 function start() {
-    var theSelect = document.getElementById('coins');
-    fetchData(theSelect);
-    theSelect.onchange = handleSelect;
+    const theSelect = document.getElementById('coins');
+
+    fetchData(theSelect, myArr);
+    theSelect.onchange = handleSelect;    
 }
 
-function fetchData(theSelect) {
+function fetchData(theSelect, myArr) {
     fetch('https://api.coincap.io/v2/assets')
         .then(response =>  {
             if (!response.ok) {
@@ -17,12 +20,18 @@ function fetchData(theSelect) {
             console.log(data);
 
             for (var i = 0; i < data.data.length; i++) {
-                var option = document.createElement("option")
+                myArr[i] = {
+                    "position": i,
+                    "name": data.data[i].name,
+                    "symbol": data.data[i].symbol,
+                    "supply": data.data[i].supply,
+                    "price": data.data[i].priceUsd,
+                    "change": data.data[i].vwap24Hr
+                }
+                var option = document.createElement("option");
                 option.value = i;
                 option.text = data.data[i].name;
                 theSelect.add(option);
-                console.log(option.value);
-                console.log(option.text);
             }
         })
         .catch(error => {
@@ -31,10 +40,20 @@ function fetchData(theSelect) {
     }
 
     function handleSelect() {
-        var supply = document.getElementById('supply');
+        var c = document.getElementById('coins')
 
-        console.log('switched');
+        const nameSpan = document.getElementById('name');
+        const symbolSpan = document.getElementById('symbol');
+        const supplySpan = document.getElementById('supply');
+        const priceSpan = document.getElementById('price');
+        const changeSpan = document.getElementById('change');
 
+        nameSpan.innerHTML = myArr[c.value].name;
+        symbolSpan.innerHTML = myArr[c.value].symbol;
+        supplySpan.innerHTML = parseInt(myArr[c.value].supply);
+        priceSpan.innerHTML = parseFloat(myArr[c.value].price).toFixed(2);
+        changeSpan.innerHTML = parseFloat(myArr[c.value].change).toFixed(2);
+        
     }
 
     window.onload = start;
